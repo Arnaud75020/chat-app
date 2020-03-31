08571d00eeec9015bba7c6a921f2eba1dec8974e
Arnaud Duhé <anro010@hotmail.com>
2020-03-31 17:11:27 +0100

    code updated after quest 2.1

--- src/App.js
+++ src/App.js
@@ -1,11 +1,39 @@
-import React from 'react';
-import Contact from './components/Contact.js'
+import React from "react";
+import Contact from "./components/Contact";
+import PropTypes from "prop-types";
+
+const users = [
+  {
+    name: "Sarah Lambert",
+    avatar: "https://randomuser.me/api/portraits/women/87.jpg",
+    status: true
+  },
+  {
+    name: "Rachel Alexander",
+    avatar: "https://randomuser.me/api/portraits/women/15.jpg",
+    status: false
+  },
+  {
+    name: "Marion Simmons",
+    avatar: "https://randomuser.me/api/portraits/men/77.jpg",
+    status: true
+  }
+];
+
 function App() {
   return (
     <div className="App">
-    <Contact />
+      {users.map(function(user) {
+        return <Contact {...user} />;
+      })}
+      ;
     </div>
   );
 }
 
+Contact.propTypes = {
+  userName: PropTypes.string.isRequired,
+  userStatus: PropTypes.bool.isRequired,
+  userAvatar: PropTypes.string.isRequired
+};
 export default App;
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,22 +1,17 @@
-import React, { Component } from "react";
-import './style.css';
+import React from "react";
+import "./Contact.css";
 
-class Contact extends Component {
-  render() {
-    const name = "Joy Patterson";
-    const avatar = "https://randomuser.me/api/portraits/women/24.jpg";
-    const status = true;
-
-    return (
-      <div className="Contact">
-        <img className="avatar" src={avatar} alt={name} />
-        <div>
-          <h4 className="name">{name}</h4>
-          <p className="status"><span className="status-online"></span>{status ? "online" : "offline"}</p>
-        </div>
-      </div>
-    );
-  }
-}
+const Contact = props => (
+  <div className="Contact">
+    <img className="avatar" src={props.avatar} alt={props.name} />
+    <div>
+      <h4 className="name">{props.name}</h4>
+      <p className="status">
+        <span className={props.status ? "status-online" : "status-offline"} />
+        {props.status ? "online" : "offline"}
+      </p>
+    </div>
+  </div>
+);
 
 export default Contact;
